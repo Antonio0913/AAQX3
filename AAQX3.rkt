8807179152ee30068fb8e576577e5f4ef5f614fa
Justin-Koida-SA <100797123+Justin-Koida-SA@users.noreply.github.com>
2024-10-15 16:48:44 -0700

    fixing zip error and more

--- AAQX3.rkt
+++ AAQX3.rkt
@@ -10,6 +10,7 @@
 (struct funDefC [(name : idC) (args : (Listof idC)) (body : AAQX3C)] #:transparent)
 (struct idC [(name : Symbol)] #:transparent)
 (struct appC [(name : idC) (args : (Listof AAQX3C))] #:transparent)
+(struct ifleq0? [(test : numC) (then : AAQX3C) (else : AAQX3C)] #:transparent)
 
 (define n1 (numC 1))
 (define n2 (numC 2))
@@ -37,16 +38,16 @@
     '- -))
 
 
-;;substs
+;;takes in _, _ and _ and 
 (define (subst [subs : (Listof (Listof AAQX3C))] [in : AAQX3C] [fds : (Listof funDefC)]) : AAQX3C
   (match in
   [(numC n) in]
-  [(idC s) (subst-id in subs)]
+  [(idC s) (subst-id in subs)] ;;if it finds an idC it calls subst-id then just stops?
   ;;[(appC n a) (appC n (subst-single what for a))]
   [(appC n a) (numC (interp (appC n (change-args subs a)) fds))]
   [(binopC op l r) (binopC op (subst subs l fds)
                       (subst subs r fds))]))
-
+ 
 
 (define (change-args [subs : (Listof (Listof AAQX3C))] [args : (Listof AAQX3C)]) : (Listof AAQX3C)
   (match args
@@ -55,20 +56,29 @@
     [(cons other r) (cons other (change-args subs r))]))
 
 
+;(define (zip [l1 : (Listof AAQX3C)] [l2 : (Listof AAQX3C)]) : (Listof (Listof AAQX3C))
+;  (cond
+;    [(not (= (length l1) (length l2)))
+;     (error 'zip "Number of variables and arguments do not match AAQX3")]
+;    [(empty? l1) '()]
+;      (cons (list (first l1) (first l2)) (zip (rest l1) (rest l2)))))
+
 (define (zip [l1 : (Listof AAQX3C)] [l2 : (Listof AAQX3C)]) : (Listof (Listof AAQX3C))
   (cond
-    [(not (= (length l1) (length l2)))
-     (error 'zip "Number of variables and arguments do not match AAQX3")]
+    [(not (equal? (length l1) (length l2))) (error 'zip "Number of variables and arguments do not match AAQX3")]
     [(empty? l1) '()]
-      (cons (list (first l1) (first l2)) (zip (rest l1) (rest l2)))))
+    [else (cons (list (first l1) (first l2)) (zip (rest l1) (rest l2)))]))
 
 (define (AAQX3C=? [arg1 : AAQX3C] [arg2 : AAQX3C]) : Boolean
   (equal? arg1 arg2))
-
+ 
 (define (subst-id [s : idC] [subs : (Listof (Listof AAQX3C))]) : AAQX3C
   (match subs
     ['() (error 'subst-id (format "AAQX3 found an unbound variable: ~a" s))]
-    [(cons (list what (? idC? for)) rest) what]
+    [(cons (list what (? idC? for)) rest)
+     (if (equal? s for)
+         what
+         (subst-id s rest))] ;;isnt this needed?
     [(cons _ rest) (subst-id s rest)]))
 
 
@@ -89,7 +99,7 @@
     [(squareC n) (expt (interp n fds) 2)]
     [(appC f a) (local ([define fd (get-fundef f fds)])
               (interp (subst (zip a (funDefC-args fd))
-                             (funDefC-body fd) fds) fds))]
+                             (funDefC-body fd) fds) fds))] ;;passes in the whole function body?
     [(idC _) (error 'interp "AAQX3 shouldn't get here")]))
 
 
@@ -123,8 +133,12 @@
      (if (hash-has-key? op-table op)
          (binopC op (parse l) (parse r))
          (error 'parse "Unsupported operator in AAQX3: ~a" op))]
-    [(list (? symbol? s) args ...) (appC (idC s) (map parse args))]
-    [other (error 'parse "syntax error in AAQX3, got ~e" other)]))
+    [(list (? symbol? s) args ...)
+     (if (hash-has-key? op-table s)
+         (error 'parse "Incorrect binop syntax: ~a in AAQX3" prog)
+         (appC (idC s) (map parse args)))] ;;needs to be able to parse function calls too here right?
+    [other (error 'parse "syntax error in AAQX3, got ~e" other)])) ;; when do we reach here now?
+
 
 ;;parse test cases
 
@@ -144,28 +158,36 @@
 
 
 
-;;(check-exn #rx"syntax error" (lambda () (parse '{+ 3})))
+(check-exn #rx"Incorrect binop syntax:" (lambda () (parse '{+ 3})))
 
-(check-equal? (parse '{+ 3})
-             (numC 5))
 
-(check-equal? (parse-prog '{{def addOne (x) => 
-                                     (+ x 1)}}) 
-              (list (funDefC (idC 'addOne) (list (idC 'x)) (binopC '+ (idC 'x) (numC 1)))))
+(check-equal? (parse-prog '{{def addOne (y) => 
+                                     (+ y 1)}}) 
+              (list (funDefC (idC 'addOne) (list (idC 'y)) (binopC '+ (idC 'y) (numC 1)))))
 
 (check-equal? (parse-prog '{{def oneAddOne () =>
                                      (+ 1 1)}})
-              (funDefC (idC 'oneAddOne) '() (binopC '+ (numC '1) (numC 1))))
+              (list (funDefC (idC 'oneAddOne) '() (binopC '+ (numC '1) (numC 1)))))
 
 (check-equal? (parse-prog '{{def area (w h) =>
                                        (* w h)} })
                      (list (funDefC (idC 'area) (list (idC 'w) (idC 'h)) (binopC '* (idC 'w) (idC 'h)))))
 
-(check-equal? (parse-prog '{{def addOne (x) =>
-                                     (+ x 1)} })
-              (list (funDefC (idC 'addOne) (list (idC 'x)) (binopC '+ (idC 'x) (numC 1)))))
+(check-equal? (parse-prog '{{def addOne (y) =>
+                                     (+ y 1)} })
+              (list (funDefC (idC 'addOne) (list (idC 'y)) (binopC '+ (idC 'y) (numC 1)))))
 
-(check-equal? (interp (parse '(addOne 2)) (list (funDefC (idC 'addOne) (list (idC 'x)) (binopC '+ (idC 'x) (numC 1))))) 3)
+(check-equal? (interp (parse '(addOne 2)) (list (funDefC (idC 'addOne) (list (idC 'y)) (binopC '+ (idC 'y) (numC 1))))) 3)
+
+
+(check-equal? (interp (parse
+                      '(addFunction 2 addOne))
+                      (list (funDefC (idC 'addOne) (list (idC 'y)) (binopC '+ (idC 'y) (numC 1)))
+                            (funDefC (idC 'addFunction) (list (idC 'num) (idC 'callFunc)) {binopC '+ {idC 'num} {appC {idC 'callFunc}{list {idC 'num}}}}))) 5)
+
+ 
+
+
 
 
 
