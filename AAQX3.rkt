1e33e6c62d8763c0e6d582dd3317c6626aadcaca
Antonio <achen301@calpoly.edu>
2024-10-17 07:34:43 -0700

    fix some test cases

--- AAQX3.rkt
+++ AAQX3.rkt
@@ -1,15 +1,15 @@
 #lang typed/racket
 (require typed/rackunit)
 
-;;defining the AAQX3C language
-(define-type AAQX3C (U numC binopC funDefC idC appC ifleq0?))
+;;defining the AAQZ3 language
+(define-type ExprC (U numC binopC FundefC idC appC ifleq0?))
 
 (struct numC[(n : Real)] #:transparent)
-(struct binopC[(op : Symbol) (l : AAQX3C) (r : AAQX3C)] #:transparent)
-(struct funDefC [(name : idC) (args : (Listof idC)) (body : AAQX3C)] #:transparent)
+(struct binopC[(op : Symbol) (l : ExprC) (r : ExprC)] #:transparent)
+(struct FundefC [(name : idC) (args : (Listof idC)) (body : ExprC)] #:transparent)
 (struct idC [(name : Symbol)] #:transparent)
-(struct appC [(name : idC) (args : (Listof AAQX3C))] #:transparent)
-(struct ifleq0? [(test : AAQX3C) (then : AAQX3C) (else : AAQX3C)] #:transparent)
+(struct appC [(name : idC) (args : (Listof ExprC))] #:transparent)
+(struct ifleq0? [(test : ExprC) (then : ExprC) (else : ExprC)] #:transparent)
 
 (define n1 (numC 1))
 (define n2 (numC 2))
@@ -47,7 +47,7 @@
 
 
 ;;takes in _, _ and _ and 
-(define (subst [subs : (Listof (Listof AAQX3C))] [in : AAQX3C] [fds : (Listof funDefC)]) : AAQX3C
+(define (subst [subs : (Listof (Listof ExprC))] [in : ExprC] [fds : (Listof FundefC)]) : ExprC
   (printf "in ~a\n" in)
   (match in
   [(numC n) in]
@@ -55,10 +55,12 @@
   
   [(binopC op l r) (binopC op (subst subs l fds)
                       (subst subs r fds))]
-  [(appC n a) (numC (interp (appC n (change-args subs a fds)) fds))]))
+  [(appC n a) (numC (interp (appC n (change-args subs a fds)) fds))]
+  [(ifleq0? con f1 f2) (ifleq0? (subst subs con fds) (subst subs f1 fds) (subst subs f2 fds))]))
   
 
-#;(define (change-args [subs : (Listof (Listof AAQX3C))] [args : (Listof AAQX3C)] [fds : (Listof funDefC)]) : (Listof AAQX3C)
+#;(define (change-args [subs : (Listof (Listof ExprC))] [args : (Listof ExprC)]
+                       [fds : (Listof FundefC)]) : (Listof ExprC)
   (printf "change-args: ~a\n" subs)
 (match args 
     ['() '()]
@@ -66,7 +68,8 @@
     [(cons (? appC? app) r) (cons (subst subs app fds) (change-args subs r fds))]
     [(cons other r) (cons other (change-args subs r fds))])) 
 
- (define (change-args [subs : (Listof (Listof AAQX3C))] [args : (Listof AAQX3C)] [fds : (Listof funDefC)]) : (Listof AAQX3C)
+ (define (change-args [subs : (Listof (Listof ExprC))] [args : (Listof ExprC)]
+                      [fds : (Listof FundefC)]) : (Listof ExprC)
    (match args 
     ['() '()]
     [(cons (? idC? id) r) 
@@ -75,51 +78,51 @@
      (cons (subst subs other fds) (change-args subs r fds))]))
 
 
-(define (zip [l1 : (Listof AAQX3C)] [l2 : (Listof AAQX3C)]) : (Listof (Listof AAQX3C))
+(define (zip [l1 : (Listof ExprC)] [l2 : (Listof ExprC)]) : (Listof (Listof ExprC))
   (match (list l1 l2)
     [(list '() '()) '()]
     [(list (cons f1 r1) (cons f2 r2)) (cons (list f1 f2) (zip r1 r2))]
-    [other (error 'zip "Number of variables and arguments do not match AAQX3")]))
+    [other (error 'zip "Number of variables and arguments do not match AAQZ3")]))
 
-#;(define (AAQX3C=? [arg1 : AAQX3C] [arg2 : AAQX3C]) : Boolean
+#;(define (ExprC=? [arg1 : ExprC] [arg2 : ExprC]) : Boolean
   (equal? arg1 arg2))
  
-(define (subst-id [s : idC] [subs : (Listof (Listof AAQX3C))] [fds : (Listof funDefC)]) : AAQX3C
+(define (subst-id [s : idC] [subs : (Listof (Listof ExprC))] [fds : (Listof FundefC)]) : ExprC
   (match subs
-    ['() (error 'subst-id (format "AAQX3 found an unbound variable: ~a" s))]
+    ['() (error 'subst-id (format "AAQZ3 found an unbound variable: ~a" s))]
     [(cons (list what (? idC? for)) rest)
      (if (equal? s for)
          (numC (interp what fds))
-         (subst-id s rest fds))]
-    [(cons _ rest) (subst-id s rest fds)]))
+         (subst-id s rest fds))]))
+    ;;[(cons _ rest) (subst-id s rest fds)]))
 
  
 
 ;;function
-(define (get-fundef [n : idC] [fds : (Listof funDefC)]) : funDefC
+(define (get-fundef [n : idC] [fds : (Listof FundefC)]) : FundefC
   (match fds
     ['() (error 'get-fundef "reference to undefined function AAQZ: ~a" n)]
     [(cons first-fd rest-fds) (cond
-                   [(equal? n (funDefC-name first-fd)) first-fd]
+                   [(equal? n (FundefC-name first-fd)) first-fd]
                    [else (get-fundef n rest-fds)])]))
 
  
 ;;interpret functions
-(define (interp-fns [funs : (Listof funDefC)]) : Real
-  (interp (funDefC-body (find-main funs)) funs))
+(define (interp-fns [funs : (Listof FundefC)]) : Real
+  (interp (FundefC-body (find-main funs)) funs))
 
-(define (find-main [funs : (Listof funDefC)]) : funDefC
+(define (find-main [funs : (Listof FundefC)]) : FundefC
   (match funs
     ['() (error 'find-main "AAQZ3C cant find main :(")]
     [(cons fun rest)
-     (if (equal? (funDefC-name fun) (idC 'main)) fun (find-main rest))]))
+     (if (equal? (FundefC-name fun) (idC 'main)) fun (find-main rest))]))
 
  
 
 (define (top-interp [funcs : Sexp])
   (interp-fns (parse-prog funcs)))
 
-(define (interp [a : AAQX3C] [fds : (Listof funDefC)]) : Real 
+(define (interp [a : ExprC] [fds : (Listof FundefC)]) : Real 
   (match a
     [(numC n) n]
     [(binopC op l r) ((hash-ref op-table op) (interp l fds) (interp r fds))]
@@ -128,9 +131,9 @@
          (interp then fds)
          (interp else fds))]
     [(appC f a) (local ([define fd (get-fundef f fds)])
-              (interp (subst (zip a (funDefC-args fd))
-                             (funDefC-body fd) fds) fds))] ;;passes in the whole function body?
-    [(idC _) (error 'interp "AAQX3 shouldn't get here got ~a" a )]))
+              (interp (subst (zip a (FundefC-args fd))
+                             (FundefC-body fd) fds) fds))] 
+    [(idC _) (error 'interp "AAQZ3 shouldn't get here got ~a" a )]))
  
 
 
@@ -142,46 +145,49 @@
 (check-equal? (interp (ifleq0? (numC 2) (numC 1) (numC 3)) '()) 3)
 (check-equal? (interp (ifleq0? (numC -2) (numC 1) (numC 3)) '()) 1)
 
-(check-equal? (interp (ifleq0? (binopC '- (numC 2) (numC 4)) (numC 1) (numC 3)) '()) 1)
-(check-equal? (interp (ifleq0? (binopC '- (numC 2) (numC 4)) (binopC '* (numC 2) (numC 4)) (numC 3)) '()) 8)
+(check-equal? (interp (ifleq0?
+                       (binopC '- (numC 2) (numC 4)) (numC 1) (numC 3)) '()) 1)
+(check-equal? (interp (ifleq0?
+                       (binopC '- (numC 2) (numC 4)) (binopC '* (numC 2) (numC 4)) (numC 3)) '()) 8)
  
 ;;parser
 
 
-(define (parse-prog [progs : Sexp]) : (Listof funDefC)
+(define (parse-prog [progs : Sexp]) : (Listof FundefC)
   (match progs
     ['() '()]
     [(cons prog rest)
      (check-duplicate-func (parse-fundef prog) (parse-prog rest))]))
      #;(match prog
-       [(list 'def (? symbol? name) '() '=> body) (cons (funDefC (idC name) '() (parse body)) (parse-prog rest))]
+       [(list 'def (? symbol? name) '() '=> body) (cons (FundefC (idC name) '() (parse body)) (parse-prog rest))]
        [(list 'def (? symbol? name) (list args ...) '=> body)
-        (if (andmap symbol? args) (cons (funDefC (idC name) (map idC args) (parse body)) (parse-prog rest))
-             (error 'parse "AAQX3 Expected a list of symbols for arguments"))]
-       [_ (error "AAQX3 Expected 'def' with correct syntax but found something else")])  
-    ;;[_ (error "AAQX3 Invalid program format")]
+        (if (andmap symbol? args) (cons (FundefC (idC name) (map idC args) (parse body)) (parse-prog rest))
+             (error 'parse "AAQZ3 Expected a list of symbols for arguments"))]
+       [_ (error "AAQZ3 Expected 'def' with correct syntax but found something else")])  
+    ;;[_ (error "AAQZ3 Invalid program format")]
  
 ;;takes in a function and a list of function and check if its a repeated name
 
-(define (check-duplicate-func [new : funDefC] [existing : (Listof funDefC)]) : (Listof funDefC)
+(define (check-duplicate-func [new : FundefC] [existing : (Listof FundefC)]) : (Listof FundefC)
   (match existing
     ['() (cons new existing)]
     [(cons func rest)
-     (if (equal? (funDefC-name new) (funDefC-name func))
+     (if (equal? (FundefC-name new) (FundefC-name func))
          (error "AAQZ3 found a syntax error repeated function name\n") (cons func (check-duplicate-func new rest)))]))
 
 
 
  
 
-(define (parse-fundef [prog : Sexp]) : funDefC
+(define (parse-fundef [prog : Sexp]) : FundefC
   (match prog
-    #;[(list 'def (? symbol? name) (list '() '=> body)) (funDefC (idC name) '() (parse body))]
+    #;[(list 'def (? symbol? name) (list '() '=> body)) (FundefC (idC name) '() (parse body))]
     [(list 'def (? symbol? name) (list (list args ...) '=> body)) ;;make sure name is valid id
      (cond
        [(hash-has-key? invalid-table name) (error 'parse "Invalid identifier in AAQZ in parse-fundef: ~a" prog)]
        [(not (andmap symbol? args)) (error 'parse "AAQZ Expected a list of symbols for arguments got ~a" args)]
-       [else (funDefC (idC name) (check-duplicate-arg (map idC args)) (parse body))])]))
+       [else (FundefC (idC name) (check-duplicate-arg (map idC args)) (parse body))])]
+    [other (error 'parse-fundef "AAQZ found awrong function structure ~a" other)]))
 
  
 
@@ -200,26 +206,26 @@
 #;(define (op-in-table? [op : Symbol]) : Boolean
   (hash-has-key? op-table op))
 
-(define (parse [prog : Sexp]) : AAQX3C 
+(define (parse [prog : Sexp]) : ExprC 
   (match prog
     [(? real? n) (numC n)]
     [(list 'ifleq0? test then else) (ifleq0? (parse test) (parse then) (parse else))]
     [(list (? symbol? op) l r)
      (if (hash-has-key? op-table op)
          (binopC op (parse l) (parse r))
-         ;(error 'parse "Unsupported operator in AAQX3: ~a" op)
+         ;(error 'parse "Unsupported operator in AAQZ3: ~a" op)
          (if (hash-has-key? invalid-table op)
-             (error 'parse "Invalid identifier: ~a in AAQX3" prog)
+             (error 'parse "Invalid identifier: ~a in AAQZ3" prog)
              (appC (idC op) (list (parse l) (parse r)))))] ;;make sure op is valid id 
     [(list (? symbol? s) args ...)
      (if (hash-has-key? invalid-table s)
-         (error 'parse "Invalid identifier: ~a in AAQX3" prog)
+         (error 'parse "Invalid identifier: ~a in AAQZ3" prog)
          (appC (idC s) (map parse args)))]
     [(? symbol? s)
      (if (hash-has-key? invalid-table s)
-         (error 'parse "Invalid identifier: ~a in AAQX3" prog)
+         (error 'parse "Invalid identifier: ~a in AAQZ3" prog)
          (idC s))]
-    [other (error 'parse "syntax error in AAQX3, got ~e" other)])) ;; when do we reach here now?
+    [other (error 'parse "syntax error in AAQZ3, got ~e" other)])) ;; when do we reach here now?
 
 
 ;;parse test cases 
@@ -252,9 +258,10 @@
 
 (check-equal? (interp (parse
                       '(sum3 (sum3 2 1 3) (addOne 1) 3))
-                      (list (funDefC (idC 'addOne) (list (idC 'y)) (binopC '+ (idC 'y) (numC 1)))
-                            (funDefC (idC 'sum3) (list (idC 'num) (idC 'num1) (idC 'num2)) {binopC '+ {idC 'num2} {binopC '+ {idC 'num1} {idC 'num}}}))) 11) 
-(printf "_____________________________________________ \n" )
+                      (list (FundefC (idC 'addOne) (list (idC 'y)) (binopC '+ (idC 'y) (numC 1)))
+                            (FundefC (idC 'sum3) (list (idC 'num) (idC 'num1) (idC 'num2))
+                                     {binopC '+ {idC 'num2} {binopC '+ {idC 'num1} {idC 'num}}}))) 11) 
+
 
 
 
@@ -265,11 +272,12 @@
                      {def main {() => {+ {f1 1 2 3 4 {f2 (f3) (f3)}} {f2 2 (f3)}}}}})) 
       -10)
 
-(check-exn #rx"Number of variables and arguments do not match"(lambda () (zip (list (numC 2) (numC 5)) (list (numC 8)))))
-(check-exn #rx"AAQX3 found an unbound variable:" (lambda () (subst-id (idC 'test) '() '())))
+(check-exn #rx"Number of variables and arguments do not match"
+           (lambda () (zip (list (numC 2) (numC 5)) (list (numC 8)))))
+(check-exn #rx"AAQZ3 found an unbound variable:" (lambda () (subst-id (idC 'test) '() '())))
 (check-exn #rx"reference to undefined function" (lambda () (get-fundef (idC 'test2) '())))
-(check-exn #rx"interp: AAQX3 shouldn't get here" (lambda () (interp (idC 'test3) '())))
-(check-exn #rx"syntax error in AAQX3, got" (lambda () (parse "Testing")))
+(check-exn #rx"interp: AAQZ3 shouldn't get here" (lambda () (interp (idC 'test3) '())))
+(check-exn #rx"syntax error in AAQZ3, got" (lambda () (parse "Testing")))
 (check-exn #rx"Invalid identifier:" (lambda () (parse '(def 2 3))))
  
 (check-exn #rx"Invalid identifier in AAQZ in parse-fundef:" (lambda () (interp-fns
@@ -293,18 +301,23 @@
                       {def f {() => 5}} 
                       {def main {() => {+ {f} {f}}}}}))))
 
-(check-exn #rx"AAQX3 found an unbound variable" (lambda () (interp-fns
+(check-exn #rx"AAQZ3 found an unbound variable" (lambda () (interp-fns
         (parse-prog '{{def f1 {(x) => {+ x y}}} 
                      {def main {() => {f1 1}}}}))))
 
-(check-exn #rx"Number of variables and arguments do not match AAQX3" (lambda () (interp-fns
+(check-exn #rx"Number of variables and arguments do not match AAQZ3" (lambda () (interp-fns
         (parse-prog '{{def f1 {(x) => {+ x y}}} 
                      {def main {() => {f1 1 2}}}}))))
+
+(check-exn #rx"AAQZ found awrong function structure" (lambda () (interp-fns
+        (parse-prog '{{def f1 {(x) => {+ x y}}} 
+                     {def main {() => {f1 1 2} 2}}}))))
 
 (check-equal? (top-interp '{{def f2 {(x y) => {* x y}}}
                      {def f1 {(x y z a b) => {- {f2 {+ x y} z} {+ a b}}}}
                      {def f3 {() => 5}}
                      {def main {() => {+ {f1 1 2 3 4 {f2 (f3) (f3)}} {f2 2 (f3)}}}}})
       -10)
+(check-exn #rx"Invalid identifier:" (lambda () (parse 'ifleq0?)))
 
- 
+(check-equal? (top-interp (quote ((def main (() => (+ (f 13) (f 0)))) (def f ((qq) => (ifleq0? qq qq (+ qq 1))))))) 14)
